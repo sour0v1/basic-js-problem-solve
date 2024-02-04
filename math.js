a97dbbd2993e39b7935cfec1a32497984effb510
Sourov <sourovdas730@gmail.com>
2024-02-04 10:36:42 +0600

    added math method

---
+++ math.js
@@ -0,0 +1,21 @@
+// math 
+
+const max = Math.max(12, 8, 9, 4, 3);
+const min = Math.min(12, 8, 9, 4, 3);
+// console.log(max);
+// console.log(min);
+
+// console.log(Math.round(2.9));
+// console.log(Math.round(2.4));
+
+// console.log(Math.abs(4-2));
+// console.log(Math.abs(2 - 4));
+
+// console.log(Math.floor(4.9));
+// console.log(Math.floor(4.1));
+
+// console.log(Math.ceil(7.2));
+
+console.log(Math.random());
+console.log(Math.random() * 10);
+console.log(Math.round(Math.random() * 10));
